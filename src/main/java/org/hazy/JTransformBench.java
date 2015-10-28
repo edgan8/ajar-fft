788fa9e5c6b5ce71281d0c1797764d2303e6f6c4
Edward Gan <edgan8@gmail.com>
2015-10-27 20:25:59 -0700

    Cleaned up some benchmarks

--- src/main/java/org/hazy/JTransformBench.java
+++ src/main/java/org/hazy/JTransformBench.java
@@ -1,32 +1,44 @@
 package org.hazy;
 
+import org.jtransforms.fft.DoubleFFT_1D;
+import pl.edu.icm.jlargearrays.ConcurrencyUtils;
+
 import java.util.Random;
 
-public class FFTAJ3Bench {
+public class JTransformBench {
     public static void main(String[] args) throws Exception {
         canFFTLarge();
         System.exit(0);
     }
 
     public static void canFFTLarge() throws Exception {
-        int n = 1<<24;
-        double[] waveSamples = new double[n];
+        int n = 1 << 26;
+
+        Random r = new Random();
+
+        double[] waveSamples = new double[2*n];
         for (int i = 0; i < n; i++) {
-            waveSamples[i] = Math.cos(
+            waveSamples[2*i] = Math.cos(
                     (double) 2*Math.PI*i / n
             ) / n;
+            /*
+            waveSamples[2*i] = r.nextDouble();
+            */
         }
+        ConcurrencyUtils.setNumberOfThreads(1);
 
-        FFTAJ3 fft = new FFTAJ3(n);
+        DoubleFFT_1D fft = new DoubleFFT_1D(n);
         long startTime = System.currentTimeMillis();
 
-        double[] freqSamples = fft.realForward(waveSamples);
+        fft.complexForward(waveSamples);
+        double[] freqSamples = waveSamples;
 
         long endTime = System.currentTimeMillis();
+        System.out.println("Took: " + Long.toString(endTime - startTime));
+
         System.out.println("First 10 freqs:");
         for (int i = 0; i < 10; i++) {
             System.out.println(freqSamples[2*i]+":"+freqSamples[2*i+1]);
         }
-        System.out.println("Took: "+Long.toString(endTime - startTime));
     }
 }
